2c7dbf45141daa830359eb4d897007bed36887cb
Igor Drobiazko <igor@elastic.io>
2016-11-07 08:45:19 +0100

    Provided some docs

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -3,6 +3,14 @@ const request = require('request-promise');
 
 module.exports = verify;
 
+/**
+ * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
+ * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
+ *
+ * @param credentials object to retrieve apiKey from
+ *
+ * @returns Promise sending HTTP request and resolving its response
+ */
 function verify(credentials) {
 
     // access the value of the apiKey field defined in credentials section of component.json
