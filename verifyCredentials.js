70b59bef14e3aedffa803469e08587be9d839f50
Olha Virolainen <olga.shukel@gmail.com>
2020-12-07 13:08:07 +0200

    test verifyCreds

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,5 +1,6 @@
 module.exports = async function verify(credentials) {
-  const { apiKey } = credentials;
+  const { apiKey, apiKeyOptional } = credentials;
   if (!apiKey) throw new Error('Error from exception: API key is missing');
+  if (!apiKeyOptional) throw new Error('Error from exception: apiKeyOptional is missing');
   return true;
 };
