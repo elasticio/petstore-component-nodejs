26342a6189f1bebe2b1fc0d8d8fbcf8c367bfd56
Pavlo Voropaiev <pavel.voropaiev@elastic.io>
2025-05-30 16:47:05 +0300

    try

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,31 +1,29 @@
-const MagentoClient = require('./lib/petstoreClient');
-
-/**
- * Executes the verification logic by sending a simple to the
- * Petstore API using the provided apiKey.
- * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
- *
- * @param credentials object to retrieve apiKey from
- *
- * @returns boolean of whether or not the request was successful
- */
 module.exports = async function verify(credentials) {
-  const { apiKey } = credentials;
+  const { apiKey, tenantId, resourceServerUrl } = credentials;
 
   if (!apiKey) throw new Error('API key is missing');
-
-  const client = new MagentoClient(this, credentials);
+  if (!tenantId) throw new Error('Tenant ID is missing');
+  if (!resourceServerUrl) throw new Error('Resource server URL is missing');
 
   try {
-    // sending a request to the most simple endpoint of the target API
-    await client.makeRequest({
-      url: '/user/me',
-      method: 'GET',
-    });
-
-    // if the request succeeds, we can assume the api key is valid
+    this.logger.info('The credentials: ', apiKey, tenantId, resourceServerUrl);
+    console.log('The credentials: ', apiKey, tenantId, resourceServerUrl);
+    this.logger.info('request succeeded');
     return true;
   } catch (e) {
-    return false;
+    this.logger.info('catch block');
+    this.logger.info('response', e.response);
+    this.logger.info('status', e.response.status);
+    if (e.response) {
+      this.logger.info('e.response block');
+      const { status } = e.response;
+      if (status === 400) {
+        this.logger.info('status 400 block');
+        return true;
+      }
+    }
+    this.logger.info('other codes error block');
+    // Other cases (no response, status other than 2xx or 400)
+    throw new Error('Verification failed');
   }
 };
